87b548d17728800bee08a4fdb2563bcbc746319e
Ying Liang <liangy.mu@gmail.com>
2021-02-15 17:31:00 -0500

    site config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://threeredthreads.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://threeredthreads.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
